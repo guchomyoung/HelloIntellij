039e65e963b594c030ad1ae9fffcd8d9af48c365
Naver <gucho@naver.com>
2016-06-27 17:22:49 +0900

    Hello IntelliJ!!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,8 @@ public class Main {
         
         // write my code here
         System.out.println("Hello World!!");
+
+        System.out.println("Hello! Local repository");
         
     }
 }
