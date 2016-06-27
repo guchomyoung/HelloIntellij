e5d7f0c77e9f5ed8143cb869c1548b6e08f040dc
guchomyoung <gucho@naver.com>
2016-06-27 17:09:57 +0900

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello IntelliJ!!!!");
+        
+        // write my code here
+        System.out.println("Hello World!!");
+        
     }
 }
